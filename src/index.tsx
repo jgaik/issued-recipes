4e74c7114812104ab42cb335098f294af0fc48ca
jgaik <jakub.gaik@gmail.com>
2025-01-15 09:34:32 +0100

    try has router

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import "@yamori-design/styles/dist/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="issued-recipes">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
